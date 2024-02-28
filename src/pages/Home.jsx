3bba494e2c9a5cc0af953f0ee2195b17c15667dd
keniaDE <decoteaukenia@gmail.com>
2024-02-27 21:51:10 -0500

    updated home page + css styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import "../Home.css"
 
 function Home(props) {
     return (
         <div>
-            <h1> Home Page </h1>
+            <header className='headerHome'>
+                <h1> FaceLess </h1>
+            </header>
+            <img src='https://i.pinimg.com/originals/af/bc/32/afbc323e2ab4be2d6d4cf7bb7ea38b20.jpg' alt='Melanin Makeup' />
+            <p> Makeup Site With Products For Melanated Skin </p>
         </div>
     );
 }
